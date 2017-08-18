ab62893b1ae541b15af9c02ab574b0f272d0dddb
Miguel Quispe <miguel.quispe@fractal.com.pe>
2017-08-18 16:53:00 -0500

    21. Formulario de registro

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -6,11 +6,23 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use BackendBundle\Entity\User; //entidad
+use AppBundle\Form\RegisterType; //formulario
+
 class UserController extends Controller
 {
     public function loginAction(Request $request){
         return $this->render('AppBundle:User:login.html.twig',array(
             "titulo" => "prueba"
+        ));
+    }
+    
+    public function registerAction(Request $request){
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+        
+        return $this->render('AppBundle:User:register.html.twig',array(
+            "form" => $form->createView()
         ));
     }
 }
