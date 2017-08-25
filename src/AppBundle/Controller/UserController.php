23566a0da325db0c6fd6670be3103ce8619dbf0f
Miguel Quispe <miguel.quispe@fractal.com.pe>
2017-08-25 11:13:38 -0500

    23. Guardar usuario registrado

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -21,6 +21,40 @@ class UserController extends Controller
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
         
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            if($form->isValid()){
+                $em = $this->getDoctrine()->getManager();
+                //$user_repo = $em->getRepository("BackendBundle:User");
+                $query = $em->createQuery('SELECT u FROM BackendBundle:User u WHERE u.email = :email OR u.nick = :nick ')
+                        ->setParameter('email',$form->get("email")->getData())
+                        ->setParameter('nick',$form->get("nick")->getData());
+                $user_isset = $query->getResult();
+                
+                if(count($user_isset) == 0){
+                    $factory = $this->get("security.encoder_factory");
+                    $encoder = $factory->getEncoder($user);
+                    $password = $encoder->encodePassword($form->get("password")->getData(),$user->getSalt());
+                    
+                    $user->setPassword($password);
+                    $user->setRole("ROLE_USER");
+                    $user->setImage(null);
+                    
+                    $em->persist($user);
+                    $flush = $em->flush();
+                    
+                    if($flush == null){
+                        $status = "Te has registrad correctamente";
+                        return $this->redirect("login");
+                    }else{
+                        $status = "No te has registrado correctamente";
+                    }
+                }else{
+                    $status = "El usuario ya existe !!";
+                }
+            }
+        }
+        
         return $this->render('AppBundle:User:register.html.twig',array(
             "form" => $form->createView()
         ));
