64d73fe21b76d3cf2f6405be64a9c7e3475afd75
Miguel Quispe <miguel.quispe@fractal.com.pe>
2017-09-18 16:07:16 -0500

    60. Ya no me gusta (unlike,dislike)

--- src/AppBundle/Controller/LikeController.php
+++ src/AppBundle/Controller/LikeController.php
@@ -6,37 +6,55 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
 use BackendBundle\Entity\Publication; //(1)
 use BackendBundle\Entity\User;
 use BackendBundle\Entity\Like;
-
 use Symfony\Component\HttpFoundation\Session\Session; //session
 
-class LikeController extends Controller{
-    
-    public function likeAction($id=null){
+class LikeController extends Controller {
+
+    public function likeAction($id = null) {
         $user = $this->getUser();
         $em = $this->getDoctrine()->getManager();
         $publication_repo = $em->getRepository("BackendBundle:Publication");
         $publication = $publication_repo->find($id);
-        
+
         $like = new Like();
         $like->setUser($user);
         $like->setPublication($publication);
-        
+
         $em->persist($like);
         $flush = $em->flush();
-        
-        if($flush == null){
+
+        if ($flush == null) {
             $status = 'Tegusta esta blicacion!!';
-        }else{
+        } else {
             $status = 'No se ha podido guardar el me gusta!!';
         }
-        
+
         return new Response($status);
-        
     }
+
+    public function unlikeAction($id = null) {
+        $user = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
    
-    
+        $like_repo = $em->getRepository("BackendBundle:Like");
+        $like = $like_repo->findOneBy(array(
+            "user" => $user,
+            "publication" => $id
+        ));
+
+        $em->remove($like);
+        $flush = $em->flush();
+
+        if($flush == null){
+            $status = 'like se ha borrado correctamente';
+        }else{
+            $status = 'like no se ha borrado';
+        }
+
+        return new Response($status);
+    }
+
 }
