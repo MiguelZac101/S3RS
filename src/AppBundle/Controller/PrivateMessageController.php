16d8906f7d681db7e9d3f45d5216d27c3f066d4f
Miguel Quispe <miguel.quispe@fractal.com.pe>
2017-11-08 17:54:24 -0500

    76. Sección de mensajes privados.

---
+++ src/AppBundle/Controller/PrivateMessageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session; //session
+
+use BackendBundle\Entity\User; //entidad
+use BackendBundle\Entity\PrivateMessage;
+
+class PrivateMessageController extends Controller {
+
+    private $session;
+
+    public function __construct() {
+        $this->session = new Session();
+    }
+
+    public function indexAction(Request $request) {
+        return $this->render('AppBundle:PrivateMessage:index.html.twig', array(
+            "titulo" => "MENSAJE DE PRUEBA"
+        ));
+    }
+
+}
