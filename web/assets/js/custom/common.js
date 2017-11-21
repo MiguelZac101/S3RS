c4fd70e0dead66dfcc1e8ba364b7233f42c2c5cf
Miguel Quispe <miguel.quispe@fractal.com.pe>
2017-11-21 15:44:24 -0500

    82. Notificaciones de MD

--- web/assets/js/custom/common.js
+++ web/assets/js/custom/common.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
+    
     if($(".label-notifications").text()==0){
         $(".label-notifications").addClass("hidden");
     }else{
         $(".label-notifications").removeClass("hidden");
+    }
+    
+    if($(".label-notifications-msg").text()==0){
+        $(".label-notifications-msg").addClass("hidden");
+    }else{
+        $(".label-notifications-msg").removeClass("hidden");
     }
     
     notifications();
@@ -28,4 +35,19 @@ function notifications(){
             }
         }
     });
+    
+    $.ajax({
+        url: URL+'/private-message/notification/get',
+        type: 'GET',
+        success: function (response){
+            $(".label-notifications-msg").html(response);
+            
+            if(response==0){
+                $(".label-notifications-msg").addClass("hidden");
+            }else{
+                $(".label-notifications-msg").removeClass("hidden");
+            }
+        }
+    });
+    
 }
